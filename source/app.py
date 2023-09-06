90e824b881fd479475e6c3163970ea673185d414
Beas <89128576+Beas-github@users.noreply.github.com>
2023-09-06 19:58:25 +0900

    Update app.py

--- source/app.py
+++ source/app.py
@@ -1,15 +1,13 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask
 
-#Flask 객체 인스턴스 생성
+# Flask 애플리케이션을 생성합니다.
 app = Flask(__name__)
 
-@app.route('/') # 접속하는 url
-def index():
-  return render_template('index.html')
+# '/' 경로로 들어오는 요청을 처리합니다.
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-if __name__=="__main__":
-  app.run(host="127.0.0.1", port="5000", debug=True)
-  #app.run(debug=True)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+if __name__ == '__main__':
+    # 개발 서버를 실행합니다.
+    app.run()
